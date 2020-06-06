804bd7f58f1654f18ab338aa1d05093857fa3591
Phuong Quach <phuongkittyquach@gmail.com>
2020-06-07 01:37:59 +0200

    Added test start screen

--- src/screens/startScreen.js
+++ src/screens/startScreen.js
@@ -1,1 +1,21 @@
-export default function () {}
+export default function () {
+  background("black");
+  fill(21, 21, 21, 150);
+  rectMode(CENTER);
+  rect(width / 2, height / 2 - 10, 300, 100, 30);
+  fill("white");
+  textSize(50);
+  textAlign(CENTER);
+  text("open vindu", width / 2, height / 2);
+}
+
+class Button {
+  constructor(x, y, width, height) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+  }
+
+  onClick() {}
+}
