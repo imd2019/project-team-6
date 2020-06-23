beec566cfed25fd5122aba7a2eee67575bbfc5a7
LynnDunkel <58940013+LynnDunkel@users.noreply.github.com>
2020-06-23 13:18:45 +0200

    Question hat jetzt eine Klick funktion. Funktioniert auch. die parameter zum welchel der icons müssen noch eingebaut werden bzw in die click funktion übergeben werden

--- src/game.js
+++ src/game.js
@@ -22,7 +22,7 @@ let coronaQuestion = [
   ),
   new Question("Möchtest du dir eine Maske kaufen?", [
     new Choice(
-      "Nein, ich brauche keine.",
+      "Ich brauche keine.",
       "Du siehst die Gefahr noch nicht und tust das als unnötig ab.",
       0,
       0,
@@ -38,7 +38,7 @@ let coronaQuestion = [
       1
     ),
     new Choice(
-      "Nein, ich werde mir eine nähen.",
+      "Ich nähe mir eine.",
       [
         "Du bist stolz auf dich, weil du es geschafft hast, Geld gespart hast und geschützt bist.",
         "Du bist enttäuscht: Es hat nicht geklappt, du hast Zeit verschwendet und bist nicht geschützt.",
--- src/screens/gameScreen.js
+++ src/screens/gameScreen.js
@@ -57,6 +57,7 @@ function draw() {
 function mouseClicked() {
   runButton.mouseClicked();
   icons.animate(-25, -25, 5);
+  // getCurrentEvent().question.mouseClicked();
 }
 
 export default { draw, mouseClicked, onStart };
