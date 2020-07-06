a3c8fdc0c00034409e75c7e653982d7050510522
LynnDunkel <58940013+LynnDunkel@users.noreply.github.com>
2020-07-06 15:58:43 +0200

    tutorial behoben

--- src/model/tutorial.js
+++ src/model/tutorial.js
@@ -29,44 +29,80 @@ export class Tutorial {
       textSize(12);
       textFont(mainFont);
       textAlign(CENTER);
+
       //frage box
+      fill(222, 70, 90);
+      noStroke();
+      text("Die Fragen", this.x + 300, this.y - 60);
       noFill();
+      rect(this.x + 300, this.y + 120, 650, 220, 50);
       strokeWeight(2);
       stroke(222, 70, 90);
-      text("Die Fragen", this.x + 300, this.y - 60);
-      rect(this.x + 300, this.y + 120, 650, 220, 50);
       line(this.x + 300, this.y + 10, this.x + 300, this.y - 50);
 
       //choices
+      noFill();
       rect(this.x + 472, this.y + 170, 135, 30, 5);
+      stroke(222, 70, 90);
+      fill(222, 70, 90);
+      noStroke();
       text("Die Antwortmöglichkeiten", this.x + 300, this.y + 260);
+      noFill();
+      strokeWeight(2);
+      stroke(222, 70, 90);
       rect(this.x + 150, this.y + 170, 100, 30, 5);
       line(this.x + 472, this.y + 185, this.x + 300, this.y + 250);
       line(this.x + 150, this.y + 185, this.x + 300, this.y + 250);
 
       //exit
+      noFill();
       rect(this.x + 605, this.y - 330, 50, 30, 5);
+      fill(222, 70, 90);
+      fill(222, 70, 90);
+      noStroke();
       text("Spiel ist zu schwer?", this.x + 490, this.y - 330);
-      line(this.x + 500, this.y - 330, this.x + 580, this.y - 330);
+      strokeWeight(2);
+      stroke(222, 70, 90);
+      line(this.x + 550, this.y - 330, this.x + 580, this.y - 330);
 
       //timeline
+      noFill();
       rect(this.x - 600, this.y + 320, 75, 75, 5);
+      fill(222, 70, 90);
+      noStroke();
       text("Der Tag", this.x - 600, this.y + 260);
+      strokeWeight(2);
+      stroke(222, 70, 90);
       line(this.x - 600, this.y + 270, this.x - 600, this.y + 282.25);
 
       textAlign(LEFT);
       //Geld
+      noFill();
       rect(this.x - 540, this.y - 320, 175, 50, 5);
+      fill(222, 70, 90);
+      noStroke();
       text("Dein Geld", this.x - 400, this.y - 320);
-      line(this.x - 450.25, this.y - 320, this.x - 390, this.y - 320);
+      strokeWeight(2);
+      stroke(222, 70, 90);
+      line(this.x - 450.25, this.y - 320, this.x - 410, this.y - 320);
       //happiness
+      noFill();
       rect(this.x - 540, this.y - 270, 175, 50, 5);
+      fill(222, 70, 90);
+      noStroke();
       text("Deine Zufriedenheit", this.x - 400, this.y - 270);
-      line(this.x - 450.25, this.y - 270, this.x - 390, this.y - 270);
+      strokeWeight(2);
+      stroke(222, 70, 90);
+      line(this.x - 450.25, this.y - 270, this.x - 410, this.y - 270);
       //healt
+      noFill();
       rect(this.x - 540, this.y - 220, 175, 50, 5);
+      fill(222, 70, 90);
+      noStroke();
       text("Deine Gesundheit", this.x - 400, this.y - 220);
-      line(this.x - 450.25, this.y - 220, this.x - 390, this.y - 220);
+      strokeWeight(2);
+      stroke(222, 70, 90);
+      line(this.x - 450.25, this.y - 220, this.x - 410, this.y - 220);
 
       this.okBtn.mouseOver();
       this.okBtn.display();
